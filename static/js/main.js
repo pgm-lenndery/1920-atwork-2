f4697cf4fc0d8ef4b0e048301345150409e97cbe
gdm-lenndery <32176355+gdm-1718-lenndery@users.noreply.github.com>
2019-12-11 21:56:02 +0100

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -11,18 +11,110 @@
             media: [
               {
                 src: '../images/te-koop/wingene/IMG_1252.JPG',
+                isThumb: false
               },
               {
                 src: '../images/te-koop/wingene/IMG_9624 copy.jpg',
+                isThumb: false
               },
               {
                 src: '../images/te-koop/wingene/IMG_9624-cover.jpg',
+                isThumb: false
               },
               {
                 src: '../images/te-koop/wingene/IMG_9624-cover2.jpeg',
+                isThumb: true
               },
               {
                 src: '../images/te-koop/wingene/IMG_9624.JPG',
+                isThumb: false
+              },
+            ]
+          }
+        ],
+      },
+      {
+        title: 'nieuwbouwwoning wingene',
+        intro: 'nieuwbouwwoning gelegen in een bosrijke, rustige omgeving.',
+        media: [
+          {
+            src: '../images/te-koop/oostveld/oostveld-01.jpg',
+            isThumb: true
+          },
+          {
+            src: '../images/te-koop/oostveld/oostveld-02.jpg',
+            isThumb: false
+          },
+        ],
+        properties: [
+          {
+            price: 292500,
+            sold: false,
+            media: [
+              {
+                src: '../images/te-koop/wingene/IMG_1252.JPG',
+              },
+            ]
+          }
+        ],
+      },
+      {
+        title: 'appartementen in oostveld',
+        intro: 'vijf nieuwbouwappartementen in het rustige oostveld',
+        properties: [
+          {
+            title: 'appartement 101',
+            bedrooms: '2',
+            price: 220000,
+            sold: false,
+            media: [
+              {
+                src: '../images/te-koop/wingene/IMG_1252.JPG',
+                isThumb: false,
+              },
+            ]
+          },
+          {
+            title: 'appartement 201',
+            bedrooms: '1',
+            price: 220000,
+            sold: true,
+            media: [
+              {
+                src: '../images/te-koop/wingene/IMG_1252.JPG',
+                isThumb: false,
+              },
+            ]
+          },
+        ],
+      },
+      {
+        title: 'nieuwbouwwoning wingene',
+        intro: 'nieuwbouwwoning gelegen in een bosrijke, rustige omgeving.',
+        properties: [
+          {
+            price: 292500,
+            sold: false,
+            media: [
+              {
+                src: '../images/te-koop/wingene/IMG_1252.JPG',
+                isThumb: false
+              },
+              {
+                src: '../images/te-koop/wingene/IMG_9624 copy.jpg',
+                isThumb: false
+              },
+              {
+                src: '../images/te-koop/wingene/IMG_9624-cover.jpg',
+                isThumb: false
+              },
+              {
+                src: '../images/te-koop/wingene/IMG_9624-cover2.jpeg',
+                isThumb: true
+              },
+              {
+                src: '../images/te-koop/wingene/IMG_9624.JPG',
+                isThumb: false
               },
             ]
           }
@@ -94,32 +186,27 @@
     
     cacheElements() {
       this.cardsTeKoop = document.querySelector('[data-label="te-koop-items"');
-      let tempStr, meta;
     },
 
     generateItemsTekoop() {
+      let tempStr = '', meta, tumbPath = '';
       if (this.cardsTeKoop !== null) {
-        database.premises.forEach((item) => {
-          console.log(`${item.title}`)
+        database.premises.forEach((item, index) => {
+          this.generateThumbPath(item.properties[0].media);
 
-          if (item.properties.length == 1) {
-            console.log(`\t 1 lot`)
-            if (item.properties[0].sold == true) {
-              console.log(`\t item sold`);
-              this.meta = 'verkocht';
-            } else {
-              console.log(`\t item price = ${item.properties[0].price}`);
-              this.meta = item.properties[0].price;
-            }
-          } else {
-            console.log(`\t meerdere loten`)
+          console.log(`premise ${++index} = ${item.title}`)
+          if (item.properties.length == 1) { 
+            // 1 LOT
+            item.properties[0].sold ? this.meta = 'verkocht' : this.meta = this.priceFormat('€ ',item.properties[0].price);
+          } else { 
+            // MEERDERE LOTEN
             this.meta = `${item.properties.length} loten`;
           }
 
-          this.tempStr += `
+          tempStr += `
             <div class="flex-grid-item card premise">
               <div class="card-head">
-                  <img src="../static/images/te-koop/wingene/IMG_9624-cover2.jpeg" alt="">
+                  <img src="${this.tumbPath.replace('../', '../static/')}" alt="">
                   <div class="meta">
                     ${this.meta}
                   </div>
@@ -131,8 +218,20 @@
             </div>
           `
         });
-        this.cardsTeKoop.innerHTML = this.tempStr;
+        this.cardsTeKoop.innerHTML = tempStr;
       }
+    },
+
+    generateThumbPath(input) {
+      input.forEach(r => {
+        if (r.isThumb == true) {
+          this.tumbPath = r.src;
+        }
+      })
+    },
+
+    priceFormat(prepend = '', input, append = '') {
+      return prepend + input.toString().match(/.{1,3}/g).join('.') + append;
     }
   }
 
