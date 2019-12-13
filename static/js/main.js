bf9c9bfa0d3409bdf6714626b5125a38b3271974
pgmgent-jarnvans <jarnvans@student.arteveldehs.be>
2019-12-13 15:34:55 +0100

    updates

--- static/js/main.js
+++ static/js/main.js
@@ -4,30 +4,187 @@
       {
         title: 'nieuwbouwwoning wingene',
         intro: 'nieuwbouwwoning gelegen in een bosrijke, rustige omgeving.',
+        slug: 'nieuwbouwwoning-met-3-slaapkamers-in-wildenburg-wingene',
+        content: `
+          <h2>Wonen in het groen</h2>
+          <p>Deze alleenstaande gezinswoning met <strong>3 slaapkamers</strong> is voorzien van alle comfort en kwaliteitsmaterialen.</p>
+          <p><strong>Gelijkvloers</strong><br>Een inkomhal met toilet, living met zithoek en eetkamer, geïnstalleerde keuken, een praktische berging, wasplaats,</p>
+          <p><strong>Eerste verdiep</strong><br>3 slaapkamers en een badkamer met een mooie, betegelde en ruime inloopdouche.</p>
+          <p>Extra troeven:</p>
+          <ul>
+              <li>Dubbele beglazing met draaikipsysteem</li>
+              <li>centrale verwarming op gas</li>
+              <li>buitenschrijnwerk in PVC</li>
+              <li>regenwaterput van 5000 liter</li>
+              <li>Ruime tuin.</li>
+          </ul>
+          <p>Droomt u van een eigen stek in de groene omgeving van Wingene? We helpen u graag verder. Contacteer ons op het nummer <a href="tel:+32495252263">0495 25 22 63</a> of stuur ons uw vraag via <a href="https://matthiastalloen.be/wp-admin/info@matthiastalloen.be">info@matthiastalloen.be</a>.</p>
+        `,
+        headImage: '../../static/images/te-koop/wingene/image-detail-head.jpg',
+        propertiesBuilding: [
+          {
+            name: 'Slaapkamers',
+            value: '3',
+          },
+          {
+            name: 'Badkamers',
+            value: '1',
+          },
+          {
+            name: 'Bewoonbare oppervlaktes',
+            value: '129m²',
+          },
+          {
+            name: 'Grondoppervlakte',
+            value: '412 m²',
+          },
+          {
+            name: 'E-peil',
+            value: '',
+          },
+          {
+            name: 'Verwarming',
+            value: 'Gas',
+          },
+          {
+            name: 'Oriëntatie tuin',
+            value: 'Zuid',
+          },
+          {
+            name: 'Oplevering',
+            value: '01-05-2019',
+          },
+        ],
+        address: {
+          street: 'Varendreef',
+          number: 93,
+          zipCode: 8750,
+          city: 'Wingene',
+          coordinates: {
+            lat: 51.095745,
+            lng: 3.298018,
+          }
+        },
         properties: [
           {
             price: 292500,
             sold: false,
             media: [
               {
-                src: '../images/te-koop/wingene/IMG_1252.JPG',
-                isThumb: false
-              },
-              {
-                src: '../images/te-koop/wingene/IMG_9624 copy.jpg',
-                isThumb: false
-              },
-              {
-                src: '../images/te-koop/wingene/IMG_9624-cover.jpg',
-                isThumb: false
-              },
-              {
                 src: '../images/te-koop/wingene/IMG_9624-cover2.jpeg',
-                isThumb: true
+                isThumb: true,
+                note: '',
               },
               {
-                src: '../images/te-koop/wingene/IMG_9624.JPG',
-                isThumb: false
+                src: '../../static/images/te-koop/wingene/img-1.jpg',
+                isThumb: false,
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-2.jpg',
+                isThumb: false,
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-3.jpg',
+                isThumb: false,
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-4.jpg',
+                isThumb: false,               
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-5.jpg',
+                isThumb: false,               
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-6.jpg',
+                isThumb: false,               
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-7.jpg',
+                isThumb: false,               
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-8.jpg',
+                isThumb: false,              
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-9.jpg',
+                isThumb: false,     
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-10.jpg',
+                isThumb: false,        
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-11.jpg',
+                isThumb: false,         
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-12.jpg',
+                isThumb: false,     
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-13.jpg',
+                isThumb: false,  
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-14.jpg',
+                isThumb: false,
+                
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-15.jpg',
+                isThumb: false,        
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-16.jpg',
+                isThumb: false,           
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-17.jpg',
+                isThumb: false,   
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-18.jpg',
+                isThumb: false,   
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-19.jpg',
+                isThumb: false,     
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-20.jpg',
+                isThumb: false,   
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-21.jpg',
+                isThumb: false, 
+                note: '',
+              },
+              {
+                src: '../../static/images/te-koop/wingene/img-22.jpg',
+                isThumb: false,  
+                note: '',
               },
             ]
           }
@@ -36,16 +193,6 @@
       {
         title: 'nieuwbouwwoning wingene',
         intro: 'nieuwbouwwoning gelegen in een bosrijke, rustige omgeving.',
-        media: [
-          {
-            src: '../images/te-koop/oostveld/oostveld-01.jpg',
-            isThumb: true
-          },
-          {
-            src: '../images/te-koop/oostveld/oostveld-02.jpg',
-            isThumb: false
-          },
-        ],
         properties: [
           {
             price: 292500,
@@ -54,7 +201,19 @@
               {
                 src: '../images/te-koop/wingene/IMG_1252.JPG',
               },
-            ]
+            ],
+            media: [
+              {
+                src: '../images/te-koop/oostveld/oostveld-01.jpg',
+                isThumb: true,
+                note: '',
+              },
+              {
+                src: '../images/te-koop/oostveld/oostveld-02.jpg',
+                isThumb: false,
+                note: '',
+              },
+            ],
           }
         ],
       },
@@ -71,6 +230,7 @@
               {
                 src: '../images/te-koop/wingene/IMG_1252.JPG',
                 isThumb: false,
+                note: '',
               },
             ]
           },
@@ -83,6 +243,7 @@
               {
                 src: '../images/te-koop/wingene/IMG_1252.JPG',
                 isThumb: false,
+                note: '',
               },
             ]
           },
@@ -98,23 +259,28 @@
             media: [
               {
                 src: '../images/te-koop/wingene/IMG_1252.JPG',
-                isThumb: false
+                isThumb: false,
+                note: '',
               },
               {
                 src: '../images/te-koop/wingene/IMG_9624 copy.jpg',
-                isThumb: false
+                isThumb: false,
+                note: '',
               },
               {
                 src: '../images/te-koop/wingene/IMG_9624-cover.jpg',
-                isThumb: false
+                isThumb: false,
+                note: '',
               },
               {
                 src: '../images/te-koop/wingene/IMG_9624-cover2.jpeg',
-                isThumb: true
+                isThumb: true,
+                note: '',
               },
               {
                 src: '../images/te-koop/wingene/IMG_9624.JPG',
-                isThumb: false
+                isThumb: false,
+                note: '',
               },
             ]
           }
@@ -130,21 +296,23 @@
         this.placeContactPointer();
       }
 
+      if (this.detailBreadcrumb !== null && this.detailTitle !== null && this.detailIntro !== null && this.detailContent !== null) {
+        this.generateDetailTeKoop();
+      }
+
       this.generateItemsTekoop();
     },
     
     cacheElements() {
       this.mapContactElement = document.querySelector('.map-contact'); 
       this.cardsTeKoop = document.querySelector('[data-label="te-koop-items"');
-      this.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        zoom: 12,
-        center: [3.266715, 51.128666], // , 
-      });
+      this.detailBreadcrumb = document.querySelector('[data-label="detail-breadcrumb"]');
+      this.detailTitle = document.querySelector('[data-label="detail-title"]');
+      this.detailIntro = document.querySelector('[data-label="detail-intro"]');
+      this.detailContent = document.querySelector('[data-label="detail-content"]');
+      this.detailPrice = document.querySelector('[data-label="detail-price"]');
 
       const bodyElement = document.body;
-      const lineNavElement = document.querySelector('.line-nav');
       const btnToggleElement = document.querySelector('.btn-hamburger');
       
       btnToggleElement.addEventListener('click', (ev) => {
@@ -157,14 +325,18 @@
       });
     },
     placeContactPointer() {
-      console.log(this.map);
-      let that = this;
-      this.map.on('load', function() {
-        console.log(that.map);
-        that.map.loadImage('../static/images/marker.png', function(error, image) {
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        zoom: 12,
+        center: [3.266715, 51.128666], // , 
+      });
+
+      map.on('load', function() {
+        map.loadImage('../static/images/marker.png', function(error, image) {
         if (error) throw error;
-        that.map.addImage('marker', image);
-          that.map.addLayer({
+        map.addImage('marker', image);
+          map.addLayer({
           "id": "points",
           "type": "symbol",
           "source": {
@@ -224,7 +396,84 @@
         this.cardsTeKoop.innerHTML = tempStr;
       }
     },
+    generateDetailTeKoop() {
+      const linkArray = window.location.href.split('/');
 
+      const currentDetailItemArray = database.premises.filter(premise => premise.slug === linkArray[4]);
+      const currentDetailItem = currentDetailItemArray[0];
+      console.log(currentDetailItem);
+
+      if (currentDetailItem !== undefined) {
+        this.detailBreadcrumb.innerHTML = currentDetailItem.title;
+        this.detailTitle.innerHTML = currentDetailItem.title;
+        this.detailIntro.innerHTML = currentDetailItem.intro;
+        this.detailPrice.innerHTML = this.getPriceBuilding(currentDetailItem.properties);
+
+        this.detailContent.innerHTML = `
+          <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+            <div class="detail-head-pic"> 
+                ${this.getHeadImage(currentDetailItem.properties.media)}
+            </div>
+            <div class="detail-description">
+                ${currentDetailItem.content}
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+            <div class="map"></div>
+            <div class="address-info">
+                <h4>Locatie</h4>
+                <address> 
+                    ${currentDetailItem.address.street} ${currentDetailItem.address.number} <br>
+                    ${currentDetailItem.address.zipCode} ${currentDetailItem.address.city}
+                </address>
+            </div>
+            <div class="detail-properties">
+                <table>
+                    <tbody>
+                        ${this.getPropsBuilding(currentDetailItem.propertiesBuilding)}                      
+                    </tbody>
+                </table>
+            </div>
+          </div>
+        `
+      }
+      
+    },
+    getPriceBuilding (props) {
+      let tempStr = '';
+
+      if (!props.isSold) {
+        props.forEach(prop => {
+          tempStr += `€ ${prop.price}`
+        })
+      }
+      else {
+        tempStr = 'VERKOCHT'
+      }
+
+      return tempStr;
+    },
+    getHeadImage(media) {
+      const headImage = media.filter(mediaItem => mediaItem.isHead);
+
+      if (headImage !== undefined) {
+        return `<img src="${headImage[0].src}" alt="">`
+      }
+    },
+    getPropsBuilding(buildingProps) {
+      let tempStr = '';
+      console.log(buildingProps)
+      buildingProps.forEach(buildingProp => {
+        tempStr += `
+          <tr>
+            <th>${buildingProp.name}</th>
+            <td>${buildingProp.value}</td>
+          </tr>
+        `
+      })
+
+      return tempStr;
+    },
     generateThumbPath(input) {
       input.forEach(r => {
         if (r.isThumb == true) {
